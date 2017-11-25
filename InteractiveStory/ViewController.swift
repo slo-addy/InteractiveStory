bb22f4d1d5c91ad5cd91bf86dabcadac2ec0b988
Addison Francisco <addison.francisco@Addisons-MacBook-Pro.local>
2017-11-24 16:28:54 -0800

    Added many things - Textfield for user name User can now put name in at beginning of story for added user context during the game.
    - Error handling for textfield

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -21,12 +27,60 @@ class ViewController: UIViewController {
 	}
 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		
 		if segue.identifier == "startAdventure" {
-			guard let pageController = segue.destination as? PageController else {
-				return
+			
+			do {
+				if let name = nameTextField.text {
+					if name == "" {
+						throw AdventureError.nameNotProvided
+					} else {
+						guard let pageController = segue.destination as? PageController else { return }
+						pageController.page = Adventure.story(withName: "Addison")
+					}
+				}
+			} catch AdventureError.nameNotProvided {
+				let alertController = UIAlertController(title: "Name not provided", message: "Provide to start the story", preferredStyle: .alert)
+				let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+				alertController.addAction(action)
+				
+				present(alertController, animated: true, completion: nil)
+			} catch let error {
+				fatalError("\(error.localizedDescription)")
 			}
-			pageController.page = Adventure.story
 		}
+	}
+	
+	@objc func keyboardWillShow(_ notification: Notification) {
+		if let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+			let frame = keyboardFrame.cgRectValue
+			textFieldBottomConstraint.constant = frame.size.height + 10
+			
+			UIView.animate(withDuration: 0.8) {
+				self.view.layoutIfNeeded()
+			}
+		}
+	}
+	
+	@objc func keyboardWillHide(_ notification: Notification) {
+		textFieldBottomConstraint.constant = 40
+		
+		UIView.animate(withDuration: 0.8) {
+			self.view.layoutIfNeeded()
+		}
+	}
+	
+	deinit {
+		// Must remove self as registered observer when being released from memory
+		// If your app targets iOS 9.0 and later or macOS 10.11 and later,
+		// you don't need to unregister an observer in its dealloc method
+		NotificationCenter.default.removeObserver(self)
+	}
+}
+
+extension ViewController: UITextFieldDelegate {
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
 	}
 }
